9f0baa2dee0b74a6c56c959ef2c721f9ec80ec9c
Wanderlei Hüttel <wanderlei.huttel@gmail.com>
2018-08-15 07:08:41 -0400

    Add openssl as variable

--- generate_CA.sh
+++ generate_CA.sh
@@ -3,7 +3,8 @@
 # Script to generate Bacula TLS Certificates
 # Based on article: http://www.bacula.pl/artykul/57/szyfrowanie-transmisji-danych-w-bacula/
 
-
+# Config Variables
+openssl=$(which openssl)
 ssl_dir="/opt/bacula/ssl"
 template_dir="/opt/bacula"
 keys_dir="${ssl_dir}/keys"
@@ -25,10 +26,10 @@ fi
 
 # Config Variables
 COUNTRY="BR"
-STATE="State"
-LOCALITY="City"
-ORGANIZATION="Bacula.org"
-emailAddress="admin@domain.com"
+STATE="Santa Catarina"
+LOCALITY="Sao Bento do Sul"
+ORGANIZATION="www.bacula.org"
+emailAddress="bacula@bacula.org"
 
 echo "============================================================================================================="
 echo " Script to generate Bacula TLS Certificates automated"
@@ -60,11 +61,11 @@ cd ${ssl_dir}
 
 #*** Generate CA Master Key and Cert ***
 echo "============================================================================================================="
-openssl genrsa -out ${keys_dir}/root_key.pem ${numbits}
-openssl rsa -check -noout -in ${keys_dir}/root_key.pem
-openssl req -new -x509 -batch -config ${ssl_dir}/openssl.cnf -sha256 -key ${keys_dir}/root_key.pem -days 36500 -out ${certs_dir}/root_cert.pem
-openssl x509 -text -noout -in ${certs_dir}/root_cert.pem
-openssl verify ${certs_dir}/root_cert.pem
+${openssl} genrsa -out ${keys_dir}/root_key.pem ${numbits}
+${openssl} rsa -check -noout -in ${keys_dir}/root_key.pem
+${openssl} req -new -x509 -batch -config ${ssl_dir}/openssl.cnf -sha256 -key ${keys_dir}/root_key.pem -days 36500 -out ${certs_dir}/root_cert.pem
+${openssl} x509 -text -noout -in ${certs_dir}/root_cert.pem
+${openssl} verify ${certs_dir}/root_cert.pem
 touch ${index_txt}
 touch ${index_attr}
 echo "01" > ${serial}
@@ -77,12 +78,12 @@ CN="localhost"
 echo "Inform IP address used from Director (bacula-dir) | [usually: localhost]:"
 read -p "IP address or FQDN: " -e -i $CN CN
 
-openssl genrsa -out ${keys_dir}/bacula-dir_key.pem ${numbits}
-openssl rsa -check -noout -in ${keys_dir}/bacula-dir_key.pem
-openssl req -new -config ${ssl_dir}/openssl.cnf -sha256 -batch -subj "/C=${COUNTRY}/ST=${STATE}/L=${LOCALITY}/O=${ORGANIZATION}/CN=${CN}/emailAddress=${emailAddress}" -key ${keys_dir}/bacula-dir_key.pem -out ${certs_dir}/bacula-dir_cert.csr
-openssl ca -keyfile ${keys_dir}/root_key.pem -config ${ssl_dir}/openssl.cnf -batch -policy policy_anything -extensions usr_cert -enddate ${end_date} -out ${certs_dir}/bacula-dir_cert.pem -infiles ${certs_dir}/bacula-dir_cert.csr
-openssl x509 -text -noout -in ${certs_dir}/bacula-dir_cert.pem
-openssl verify ${certs_dir}/bacula-dir_cert.pem
+${openssl} genrsa -out ${keys_dir}/bacula-dir_key.pem ${numbits}
+${openssl} rsa -check -noout -in ${keys_dir}/bacula-dir_key.pem
+${openssl} req -new -config ${ssl_dir}/openssl.cnf -sha256 -batch -subj "/C=${COUNTRY}/ST=${STATE}/L=${LOCALITY}/O=${ORGANIZATION}/CN=${CN}/emailAddress=${emailAddress}" -key ${keys_dir}/bacula-dir_key.pem -out ${certs_dir}/bacula-dir_cert.csr
+${openssl} ca -keyfile ${keys_dir}/root_key.pem -config ${ssl_dir}/openssl.cnf -batch -policy policy_anything -extensions usr_cert -enddate ${end_date} -out ${certs_dir}/bacula-dir_cert.pem -infiles ${certs_dir}/bacula-dir_cert.csr
+${openssl} x509 -text -noout -in ${certs_dir}/bacula-dir_cert.pem
+${openssl} verify ${certs_dir}/bacula-dir_cert.pem
 
 
 
@@ -93,11 +94,11 @@ echo "Inform IP address used from FileDaemon (bacula-fd) | [usually: localhost]:
 read -p "IP address or FQDN: " -e -i $CN CN
 
 serial=$(cat serial)
-openssl genrsa -out ${keys_dir}/bacula-fd_key.pem ${numbits}
-openssl req -new -config ${ssl_dir}/openssl.cnf -sha256 -batch -subj "/C=${COUNTRY}/ST=${STATE}/L=${LOCALITY}/O=${ORGANIZATION}/CN=${CN}/emailAddress=${emailAddress}" -key ${keys_dir}/bacula-fd_key.pem -out ${certs_dir}/bacula-fd_cert.csr
-openssl ca -keyfile ${keys_dir}/root_key.pem -config ${ssl_dir}/openssl.cnf -batch -policy policy_anything -extensions usr_cert -enddate ${end_date} -out ${certs_dir}/bacula-fd_cert.pem -infiles ${certs_dir}/bacula-fd_cert.csr
-openssl x509 -text -noout -in ${certs_dir}/bacula-fd_cert.pem
-openssl verify ${certs_dir}/bacula-fd_cert.pem
+${openssl} genrsa -out ${keys_dir}/bacula-fd_key.pem ${numbits}
+${openssl} req -new -config ${ssl_dir}/openssl.cnf -sha256 -batch -subj "/C=${COUNTRY}/ST=${STATE}/L=${LOCALITY}/O=${ORGANIZATION}/CN=${CN}/emailAddress=${emailAddress}" -key ${keys_dir}/bacula-fd_key.pem -out ${certs_dir}/bacula-fd_cert.csr
+${openssl} ca -keyfile ${keys_dir}/root_key.pem -config ${ssl_dir}/openssl.cnf -batch -policy policy_anything -extensions usr_cert -enddate ${end_date} -out ${certs_dir}/bacula-fd_cert.pem -infiles ${certs_dir}/bacula-fd_cert.csr
+${openssl} x509 -text -noout -in ${certs_dir}/bacula-fd_cert.pem
+${openssl} verify ${certs_dir}/bacula-fd_cert.pem
 
 
 
@@ -108,11 +109,11 @@ echo "Inform IP address used from Storage Daemon (bacula-sd) | [usually: ${CN}]:
 read -p "IP address or FQDN: " -e -i $CN CN
 
 serial=$(cat serial)
-openssl genrsa -out ${keys_dir}/bacula-sd_key.pem ${numbits}
-openssl req -new -config ${ssl_dir}/openssl.cnf -sha256 -batch -subj "/C=${COUNTRY}/ST=${STATE}/L=${LOCALITY}/O=${ORGANIZATION}/CN=${CN}/emailAddress=${emailAddress}" -key ${keys_dir}/bacula-sd_key.pem -out ${certs_dir}/bacula-sd_cert.csr
-openssl ca -keyfile ${keys_dir}/root_key.pem -config ${ssl_dir}/openssl.cnf -batch -policy policy_anything -extensions usr_cert -enddate ${end_date} -out ${certs_dir}/bacula-sd_cert.pem -infiles ${certs_dir}/bacula-sd_cert.csr
-openssl x509 -text -noout -in ${certs_dir}/bacula-sd_cert.pem
-openssl verify ${certs_dir}/bacula-sd_cert.pem
+${openssl} genrsa -out ${keys_dir}/bacula-sd_key.pem ${numbits}
+${openssl} req -new -config ${ssl_dir}/openssl.cnf -sha256 -batch -subj "/C=${COUNTRY}/ST=${STATE}/L=${LOCALITY}/O=${ORGANIZATION}/CN=${CN}/emailAddress=${emailAddress}" -key ${keys_dir}/bacula-sd_key.pem -out ${certs_dir}/bacula-sd_cert.csr
+${openssl} ca -keyfile ${keys_dir}/root_key.pem -config ${ssl_dir}/openssl.cnf -batch -policy policy_anything -extensions usr_cert -enddate ${end_date} -out ${certs_dir}/bacula-sd_cert.pem -infiles ${certs_dir}/bacula-sd_cert.csr
+${openssl} x509 -text -noout -in ${certs_dir}/bacula-sd_cert.pem
+${openssl} verify ${certs_dir}/bacula-sd_cert.pem
 
 
 echo "done"
